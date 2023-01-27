4afe1193ac3560c0f0c55df41416bb515c9e5adc
Andrés Toledano <andres13072001@gmail.com>
2023-01-27 12:27:47 +0100

    Empiezo el proyecto

---
+++ src/components/AsistentesDirecto.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <div id="asistentes">
+        <h1>Asistentes a la sesion en directo</h1>
+        <ul>
+            <li v-for="asistente in asistentes.slice().reverse()" :key="asistente">{{ asistente }}</li>
+        </ul>
+    </div>
+    <div id="caja-form">
+        <div id="add">
+            <input type="text" placeholder="Nombre del asistente" v-model="nombre_asistente">
+            <button @click="addAsist">Añadir asistente</button>
+            <button @click="delAsist">Añadir asistente</button>
+        </div>
+    </div>
+    <cite>{{ mensaje }}</cite>
+  </section>
+</template>
+
+<script>
+export default {
+    name:'AsistentesDirecto'
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
